af18d0978e374dad440cdbabb6164da3a75cbaf0
jdotglove <glover.jarod@gmail.com>
2025-05-26 23:00:24 -0500

    fix attempt

--- src/main/java/com/nest/NestHttpServer.java
+++ src/main/java/com/nest/NestHttpServer.java
@@ -1,10 +1,12 @@
 package src.main.java.com.nest;
 // Java Program to Set up a Basic HTTP Server
 import com.sun.net.httpserver.HttpServer;
-
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
 import src.main.java.com.nest.lib.Router;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class NestHttpServer 
@@ -13,11 +15,27 @@ public class NestHttpServer
     {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
+        
         server.createContext("/", (req) -> Router.mainApiHandler(req));
+        server.createContext("/health", new HealthCheckHandler());
         server.createContext("/spotify/", (req) -> Router.spotifyApiHandler(req));
 
         server.setExecutor(null); // Use the default executor
         server.start();
         System.out.println("Server is running on port 8080");
+    }
+
+    // define a custom HttpHandler
+    static class HealthCheckHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException 
+        {
+            // handle the request
+            String response = "Hello, this is a simple HTTP server response!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
